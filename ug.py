ea91a44e5ffe75e72b3f1488e488691872fd4b1d
ribilraj <ribilrajdhakar@gmail.com>
2025-08-03 15:40:49 +0530

    Update ug.py

--- ug.py
+++ ug.py
@@ -595,7 +595,7 @@ async def send_vid(bot: Client, m: Message, cc, filename, thumb, name, prog, cha
             subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:10 -vframes 1 -q:v 2 -y "{temp_thumb}"', shell=True)
             if os.path.exists(temp_thumb):
                 spaced_text = ' '.join(watermark)
-                text_cmd = f'ffmpeg -i "{temp_thumb}" -vf "drawbox=y=0:color=black@0.5:width=iw:height=200:t=fill,drawtext=fontfile=font.otf:text=\'{spaced_text}\':fontcolor=white:fontsize=90:x=(w-text_w)/2:y=60" -c:v mjpeg -q:v 2 -y "{temp_thumb}"'
+                text_cmd = f'ffmpeg -i "{temp_thumb}" -vf "drawbox=y=0:color=black@0.5:width=iw:height=150:t=fill,drawtext=fontfile=font.otf:text=\'{spaced_text}\':fontcolor=white:fontsize=h/7:x=(w-text_w)/2:y=50" -c:v mjpeg -q:v 2 -y "{temp_thumb}"'
                 subprocess.run(text_cmd, shell=True)
             thumbnail = temp_thumb if os.path.exists(temp_thumb) else None
             
